fac5031f41fb8e0e5b7634b41ebc762f9756713e
Xl.W <shawlix+git@gmail.com>
2017-11-25 19:53:17 +0800

    aries: add qcom sepolicy dependence

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,5 +42,25 @@ TARGET_RECOVERY_FSTAB            := $(LOCAL_PATH)/rootdir/ramdisk/fstab.qcom
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 
+# Board specific SELinux policy variable definitions
+BOARD_SEPOLICY_DIRS += \
+    device/qcom/sepolicy/common \
+    device/qcom/sepolicy/ssg \
+    device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
+
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+BOARD_SEPOLICY_DIRS += \
+    device/qcom/sepolicy/test
+endif
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/qcom/sepolicy/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/qcom/sepolicy/private
+
+BOARD_SEPOLICY_DIRS += \
+       device/xiaomi/aries/sepolicy
+
 -include vendor/xiaomi/aries/BoardConfigVendor.mk
 
